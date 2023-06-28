4e5a745ba9b642a918bd742a71d09d2ca80dffaa
Aman Joshi <amanjoshi2527@gmail.com>
2023-06-29 05:08:24 +0530

    Python for learners

---
+++ Prac1.py
@@ -0,0 +1,16 @@
+#if and mactch funtions uses
+http_status=201
+if http_status==200  or http_status== 201:
+    print( 'Success')
+elif http_status ==400:
+    print('Bad request')
+elif http_status==404:
+    print( 'Not Found')
+elif http_status==500 or http_status== 501:
+  print('Server Error')
+else:
+ print('Unknown')
+ 
+match http_status:
+  case 200|201:
+   print('Success')
